a3eead28c4c5ed36e0b81080419d671dc7b6dee5
Matheus Cardoso <matheuscardosooffice@gmail.com>
2023-08-27 18:33:30 -0300

    criando função de filtro na tabela

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
 import menuReducer from './menuReducer';
+import personReducer from './PESSOAL';
 
 const rootReducer = combineReducers({
-    menu: menuReducer
+    menu: menuReducer,
+    pessoal: personReducer
 });
 
 export default rootReducer;
