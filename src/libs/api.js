b0ae8319caf02706f1378ecb59fb83f52914c2b0
Matheus Cardoso <matheuscardosooffice@gmail.com>
2023-11-05 18:25:05 -0300

    Adicionando url da api

--- src/libs/api.js
+++ src/libs/api.js
@@ -1,5 +1,6 @@
 import axios from "axios";
 
 export const api = axios.create({
-    baseURL: 'https://localhost:7221/'
+    // baseURL: 'https://localhost:7221/'
+    baseURL: 'https://erpwebapitest.azurewebsites.net/'
 })
